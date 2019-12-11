6f6c6a0f2b9bb684782405d8c6cebf1f22595e7a
Cody Garrett <cody.l.garrett22@gmail.com>
2019-12-10 17:48:44 -0700

    project complete

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Container from "./Components/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container />
+      </header>
+    </div>
+  );
+}
+
+export default App;
